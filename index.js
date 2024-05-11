97676cd230ed9d39ef8063ed53b218dca27c4857
Ramiro Cosa <ramirocosa@gmail.com>
2024-05-11 11:55:30 -0300

    retry

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const requestLogger = (request, response, next) => {
   console.log("---");
   next();
 };
+//app.use(express.static("dist"));
 app.use(requestLogger);
 
 app.get("/", (request, response) => {
@@ -82,12 +83,9 @@ app.post("/api/notes", (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
-
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
